47bb5ee5a2def2f591fb16da3001d1ad72118ac4
Rootjhon <354888562@qq.com>
2020-06-09 11:06:26 +0800

    init with 0.6.2

---
+++ Scripts/PackageLoad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UPRProfiler
+{
+    public class PackageLoad : MonoBehaviour
+    {
+        public static bool useLua = false;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void OnStartGame()
+        {
+            GameObject uprGameObject = new GameObject("UPRProfiler", typeof(InnerPackageS))
+            {
+                name = "UPRProfiler",
+                hideFlags = HideFlags.HideAndDontSave,
+            };
+            DontDestroyOnLoad(uprGameObject);
+            NetworkServer.ConnectTcpPort(56000);
+            Debug.Log("[UPRProfiler] PackageLoad OnStartGame");
+        }
+    }
+}
+
