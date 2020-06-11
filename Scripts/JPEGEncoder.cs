aedcc6de62e9aab12da0f77738865084e5ea7035
Rootjhon <354888562@qq.com>
2020-06-11 21:26:49 +0800

    just read and format code
    阅读 的过程中格式化了一下

--- Scripts/JPEGEncoder.cs
+++ Scripts/JPEGEncoder.cs
@@ -1,22 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
 using System.IO;
+using UnityEngine;
+
 #if UNITY_2018_2_OR_NEWER
 using Unity.Collections;
 #endif
+
 namespace UPRProfiler
 {
-    public class ByteArray
+    public sealed class ByteArray
     {
+        #region [Fields]
         private MemoryStream stream;
         private BinaryWriter writer;
+        #endregion
 
-
+        #region [Construct]
         public ByteArray()
         {
             stream = new MemoryStream();
             writer = new BinaryWriter(stream);
         }
+        #endregion
 
+        #region [API]
         public void writeByte(byte value)
         {
             writer.Write(value);
@@ -34,29 +41,32 @@ namespace UPRProfiler
         {
             this.stream.Dispose();
         }
+        #endregion
     }
 
-    class BitString
+    sealed class BitString
     {
         public int len = 0;
         public int val = 0;
     }
 
-    public class BitmapData
+    public sealed class BitmapData
     {
+        #region [Fields]
         public int height;
         public int width;
+        
+        private Color result = Color.clear;
+        #endregion
 
+        #region [Unity_Older]
         private byte[] rawbyte;
-        Color result = new Color();
-
         public BitmapData(byte[] rawbyte, int _width, int _height)
         {
             this.rawbyte = rawbyte;
             height = _height;
             width = _width;
         }
-
         public Color getPixelColor(int x, int y, int type)
         {
             if (x >= width)
@@ -76,17 +86,17 @@ namespace UPRProfiler
             result.b = (float)rawbyte[y * width * 3 + x * 3 + 2];
             return result;
         }
+        #endregion
 
+        #region [Unity_2018_2_OR_NEWER]
 #if UNITY_2018_2_OR_NEWER
         private NativeArray<byte> nativeRawByte;
-
         public BitmapData(NativeArray<byte> nativerawbyte, int _width, int _height)
         {
             this.nativeRawByte = nativerawbyte;
             height = _height;
             width = _width;
         }
-
         public Color getNativePixelColor(int x, int y, int type)
         {
             if (x >= width)
@@ -107,22 +117,25 @@ namespace UPRProfiler
             return result;
         }
 #endif
+        #endregion
     }
 
-    public class JPGEncoder
+    public sealed class JPGEncoder
     {
-
-        public int[] ZigZag = new int[64] {
-         0, 1, 5, 6,14,15,27,28,
-         2, 4, 7,13,16,26,29,42,
-         3, 8,12,17,25,30,41,43,
-         9,11,18,24,31,40,44,53,
-        10,19,23,32,39,45,52,54,
-        20,22,33,38,46,51,55,60,
-        21,34,37,47,50,56,59,61,
-        35,36,48,49,57,58,62,63
-    };
-        int[] YQT = new int[64] {
+        #region [Fields]
+        public int[] ZigZag = new int[64]
+        {
+             0, 1, 5, 6,14,15,27,28,
+             2, 4, 7,13,16,26,29,42,
+             3, 8,12,17,25,30,41,43,
+             9,11,18,24,31,40,44,53,
+            10,19,23,32,39,45,52,54,
+            20,22,33,38,46,51,55,60,
+            21,34,37,47,50,56,59,61,
+            35,36,48,49,57,58,62,63
+        };
+        int[] YQT = new int[64]
+        {
             16, 11, 10, 16, 24, 40, 51, 61,
             12, 12, 14, 19, 26, 58, 60, 55,
             14, 13, 16, 24, 40, 57, 69, 56,
@@ -132,7 +145,8 @@ namespace UPRProfiler
             49, 64, 78, 87,103,121,120,101,
             72, 92, 95, 98,112,100,103, 99
         };
-        int[] UVQT = new int[64] {
+        int[] UVQT = new int[64]
+        {
             17, 18, 24, 47, 99, 99, 99, 99,
             18, 21, 26, 66, 99, 99, 99, 99,
             24, 26, 56, 99, 99, 99, 99, 99,
@@ -142,7 +156,8 @@ namespace UPRProfiler
             99, 99, 99, 99, 99, 99, 99, 99,
             99, 99, 99, 99, 99, 99, 99, 99
         };
-        float[] aasf = new float[8] {
+        float[] aasf = new float[8]
+        {
             1.0f, 1.387039845f, 1.306562965f, 1.175875602f,
             1.0f, 0.785694958f, 0.541196100f, 0.275899379f
         };
@@ -151,6 +166,135 @@ namespace UPRProfiler
         private float[] fdtbl_Y = new float[64];
         private float[] fdtbl_UV = new float[64];
 
+        private BitString[] YDC_HT;
+        private BitString[] UVDC_HT;
+        private BitString[] YAC_HT;
+        private BitString[] UVAC_HT;
+        private BitString[] HT;
+        private int[] std_dc_luminance_nrcodes = new int[17] { 0, 0, 1, 5, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 };
+        private int[] std_dc_luminance_values = new int[12] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        private int[] std_ac_luminance_nrcodes = new int[17] { 0, 0, 2, 1, 3, 3, 2, 4, 3, 5, 5, 4, 4, 0, 0, 1, 0x7d };
+        private int[] std_ac_luminance_values = new int[162]
+        {
+            0x01,0x02,0x03,0x00,0x04,0x11,0x05,0x12,
+            0x21,0x31,0x41,0x06,0x13,0x51,0x61,0x07,
+            0x22,0x71,0x14,0x32,0x81,0x91,0xa1,0x08,
+            0x23,0x42,0xb1,0xc1,0x15,0x52,0xd1,0xf0,
+            0x24,0x33,0x62,0x72,0x82,0x09,0x0a,0x16,
+            0x17,0x18,0x19,0x1a,0x25,0x26,0x27,0x28,
+            0x29,0x2a,0x34,0x35,0x36,0x37,0x38,0x39,
+            0x3a,0x43,0x44,0x45,0x46,0x47,0x48,0x49,
+            0x4a,0x53,0x54,0x55,0x56,0x57,0x58,0x59,
+            0x5a,0x63,0x64,0x65,0x66,0x67,0x68,0x69,
+            0x6a,0x73,0x74,0x75,0x76,0x77,0x78,0x79,
+            0x7a,0x83,0x84,0x85,0x86,0x87,0x88,0x89,
+            0x8a,0x92,0x93,0x94,0x95,0x96,0x97,0x98,
+            0x99,0x9a,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,
+            0xa8,0xa9,0xaa,0xb2,0xb3,0xb4,0xb5,0xb6,
+            0xb7,0xb8,0xb9,0xba,0xc2,0xc3,0xc4,0xc5,
+            0xc6,0xc7,0xc8,0xc9,0xca,0xd2,0xd3,0xd4,
+            0xd5,0xd6,0xd7,0xd8,0xd9,0xda,0xe1,0xe2,
+            0xe3,0xe4,0xe5,0xe6,0xe7,0xe8,0xe9,0xea,
+            0xf1,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,0xf8,
+            0xf9,0xfa
+        };
+        private int[] std_dc_chrominance_nrcodes = new int[17] { 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
+        private int[] std_dc_chrominance_values = new int[12] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        private int[] std_ac_chrominance_nrcodes = new int[17] { 0, 0, 2, 1, 2, 4, 4, 3, 4, 7, 5, 4, 4, 0, 1, 2, 0x77 };
+        private int[] std_ac_chrominance_values = new int[162]
+        {
+            0x00,0x01,0x02,0x03,0x11,0x04,0x05,0x21,
+            0x31,0x06,0x12,0x41,0x51,0x07,0x61,0x71,
+            0x13,0x22,0x32,0x81,0x08,0x14,0x42,0x91,
+            0xa1,0xb1,0xc1,0x09,0x23,0x33,0x52,0xf0,
+            0x15,0x62,0x72,0xd1,0x0a,0x16,0x24,0x34,
+            0xe1,0x25,0xf1,0x17,0x18,0x19,0x1a,0x26,
+            0x27,0x28,0x29,0x2a,0x35,0x36,0x37,0x38,
+            0x39,0x3a,0x43,0x44,0x45,0x46,0x47,0x48,
+            0x49,0x4a,0x53,0x54,0x55,0x56,0x57,0x58,
+            0x59,0x5a,0x63,0x64,0x65,0x66,0x67,0x68,
+            0x69,0x6a,0x73,0x74,0x75,0x76,0x77,0x78,
+            0x79,0x7a,0x82,0x83,0x84,0x85,0x86,0x87,
+            0x88,0x89,0x8a,0x92,0x93,0x94,0x95,0x96,
+            0x97,0x98,0x99,0x9a,0xa2,0xa3,0xa4,0xa5,
+            0xa6,0xa7,0xa8,0xa9,0xaa,0xb2,0xb3,0xb4,
+            0xb5,0xb6,0xb7,0xb8,0xb9,0xba,0xc2,0xc3,
+            0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xd2,
+            0xd3,0xd4,0xd5,0xd6,0xd7,0xd8,0xd9,0xda,
+            0xe2,0xe3,0xe4,0xe5,0xe6,0xe7,0xe8,0xe9,
+            0xea,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,0xf8,
+            0xf9,0xfa
+        };
+
+        private BitString[] bitcode = new BitString[65535];
+        private int[] category = new int[65535];
+
+        private int bytenew = 0;
+        private int bytepos = 7;
+        public ByteArray byteout = new ByteArray();
+
+        private int[] DU = new int[64];
+        private float[] YDU = new float[64];
+        private float[] UDU = new float[64];
+        private float[] VDU = new float[64];
+        public bool isDone = false;
+        private BitmapData image;
+        private int sf = 0;
+        #endregion
+
+        #region [Construct]
+        public JPGEncoder(float quality)
+        {
+            if (quality <= 0.0f)
+                quality = 1.0f;
+
+            if (quality > 100.0f)
+                quality = 100.0f;
+
+            if (quality < 50.0f)
+                sf = (int)(5000.0f / quality);
+            else
+                sf = (int)(200.0f - quality * 2.0f);
+
+            initHuffmanTbl();
+            initCategoryfloat();
+            initQuantTables(sf);
+        }
+        #endregion
+
+        #region [API]
+        public byte[] GetBytes()
+        {
+            if (!isDone)
+            {
+                Debug.LogError("JPEGEncoder not complete, cannot get bytes!");
+                return new byte[1];
+            }
+
+            return byteout.GetAllBytes();
+        }
+        public void doEncoding(byte[] rawByte, int width, int height)
+        {
+            image = new BitmapData(rawByte, width, height);
+            isDone = false;
+            encode();
+            isDone = true;
+            image = null;
+        }
+
+#if UNITY_2018_2_OR_NEWER
+        public void doNativeEncoding(NativeArray<byte> rawByte, int width, int height)
+        {
+            image = new BitmapData(rawByte, width, height);
+            isDone = false;
+            encode();
+            isDone = true;
+            image = null;
+        }
+#endif
+        #endregion
+
+        #region [Business]
         private void initQuantTables(int sf)
         {
             int i;
@@ -169,9 +313,7 @@ namespace UPRProfiler
                 }
                 YTable[ZigZag[i]] = (int)t;
             }
-
-
-
+            
             for (i = 0; i < 64; i++)
             {
                 t = Mathf.Floor((UVQT[i] * sf + 50.0f) / 100.0f);
@@ -185,9 +327,7 @@ namespace UPRProfiler
                 }
                 UVTable[ZigZag[i]] = (int)t;
             }
-
-
-
+            
             i = 0;
             for (int row = 0; row < 8; row++)
             {
@@ -199,67 +339,6 @@ namespace UPRProfiler
                 }
             }
         }
-
-        private BitString[] YDC_HT;
-        private BitString[] UVDC_HT;
-        private BitString[] YAC_HT;
-        private BitString[] UVAC_HT;
-        private BitString[] HT;
-        private int[] std_dc_luminance_nrcodes = new int[17] { 0, 0, 1, 5, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 };
-        private int[] std_dc_luminance_values = new int[12] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-        private int[] std_ac_luminance_nrcodes = new int[17] { 0, 0, 2, 1, 3, 3, 2, 4, 3, 5, 5, 4, 4, 0, 0, 1, 0x7d };
-        private int[] std_ac_luminance_values = new int[162] {
-        0x01,0x02,0x03,0x00,0x04,0x11,0x05,0x12,
-        0x21,0x31,0x41,0x06,0x13,0x51,0x61,0x07,
-        0x22,0x71,0x14,0x32,0x81,0x91,0xa1,0x08,
-        0x23,0x42,0xb1,0xc1,0x15,0x52,0xd1,0xf0,
-        0x24,0x33,0x62,0x72,0x82,0x09,0x0a,0x16,
-        0x17,0x18,0x19,0x1a,0x25,0x26,0x27,0x28,
-        0x29,0x2a,0x34,0x35,0x36,0x37,0x38,0x39,
-        0x3a,0x43,0x44,0x45,0x46,0x47,0x48,0x49,
-        0x4a,0x53,0x54,0x55,0x56,0x57,0x58,0x59,
-        0x5a,0x63,0x64,0x65,0x66,0x67,0x68,0x69,
-        0x6a,0x73,0x74,0x75,0x76,0x77,0x78,0x79,
-        0x7a,0x83,0x84,0x85,0x86,0x87,0x88,0x89,
-        0x8a,0x92,0x93,0x94,0x95,0x96,0x97,0x98,
-        0x99,0x9a,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,
-        0xa8,0xa9,0xaa,0xb2,0xb3,0xb4,0xb5,0xb6,
-        0xb7,0xb8,0xb9,0xba,0xc2,0xc3,0xc4,0xc5,
-        0xc6,0xc7,0xc8,0xc9,0xca,0xd2,0xd3,0xd4,
-        0xd5,0xd6,0xd7,0xd8,0xd9,0xda,0xe1,0xe2,
-        0xe3,0xe4,0xe5,0xe6,0xe7,0xe8,0xe9,0xea,
-        0xf1,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,0xf8,
-        0xf9,0xfa
-    };
-        private int[] std_dc_chrominance_nrcodes = new int[17] { 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
-        private int[] std_dc_chrominance_values = new int[12] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-        private int[] std_ac_chrominance_nrcodes = new int[17] { 0, 0, 2, 1, 2, 4, 4, 3, 4, 7, 5, 4, 4, 0, 1, 2, 0x77 };
-        private int[] std_ac_chrominance_values = new int[162] {
-        0x00,0x01,0x02,0x03,0x11,0x04,0x05,0x21,
-        0x31,0x06,0x12,0x41,0x51,0x07,0x61,0x71,
-        0x13,0x22,0x32,0x81,0x08,0x14,0x42,0x91,
-        0xa1,0xb1,0xc1,0x09,0x23,0x33,0x52,0xf0,
-        0x15,0x62,0x72,0xd1,0x0a,0x16,0x24,0x34,
-        0xe1,0x25,0xf1,0x17,0x18,0x19,0x1a,0x26,
-        0x27,0x28,0x29,0x2a,0x35,0x36,0x37,0x38,
-        0x39,0x3a,0x43,0x44,0x45,0x46,0x47,0x48,
-        0x49,0x4a,0x53,0x54,0x55,0x56,0x57,0x58,
-        0x59,0x5a,0x63,0x64,0x65,0x66,0x67,0x68,
-        0x69,0x6a,0x73,0x74,0x75,0x76,0x77,0x78,
-        0x79,0x7a,0x82,0x83,0x84,0x85,0x86,0x87,
-        0x88,0x89,0x8a,0x92,0x93,0x94,0x95,0x96,
-        0x97,0x98,0x99,0x9a,0xa2,0xa3,0xa4,0xa5,
-        0xa6,0xa7,0xa8,0xa9,0xaa,0xb2,0xb3,0xb4,
-        0xb5,0xb6,0xb7,0xb8,0xb9,0xba,0xc2,0xc3,
-        0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xd2,
-        0xd3,0xd4,0xd5,0xd6,0xd7,0xd8,0xd9,0xda,
-        0xe2,0xe3,0xe4,0xe5,0xe6,0xe7,0xe8,0xe9,
-        0xea,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,0xf8,
-        0xf9,0xfa
-    };
-        private BitString[] bitcode = new BitString[65535];
-        private int[] category = new int[65535];
-
         private BitString[] computeHuffmanTbl(int[] nrcodes, int[] std_table)
         {
             int codevalue = 0;
@@ -279,7 +358,6 @@ namespace UPRProfiler
             }
             return HT;
         }
-
         private void initHuffmanTbl()
         {
             YDC_HT = computeHuffmanTbl(std_dc_luminance_nrcodes, std_dc_luminance_values);
@@ -287,7 +365,6 @@ namespace UPRProfiler
             YAC_HT = computeHuffmanTbl(std_ac_luminance_nrcodes, std_ac_luminance_values);
             UVAC_HT = computeHuffmanTbl(std_ac_chrominance_nrcodes, std_ac_chrominance_values);
         }
-
         private void initCategoryfloat()
         {
             int nrlower = 1;
@@ -321,31 +398,15 @@ namespace UPRProfiler
                 nrupper <<= 1;
             }
         }
-
-        private int bytenew = 0;
-        private int bytepos = 7;
-        public ByteArray byteout = new ByteArray();
-
-        public byte[] GetBytes()
-        {
-            if (!isDone)
-            {
-                Debug.LogError("JPEGEncoder not complete, cannot get bytes!");
-                return new byte[1];
-            }
-
-            return byteout.GetAllBytes();
-        }
-
         private void writeBits(BitString bs)
         {
             int value = bs.val;
             int posval = bs.len - 1;
             while (posval >= 0)
             {
-                if (((uint)value & System.Convert.ToUInt32(1 << posval)) != 0)
+                if (((uint)value & Convert.ToUInt32(1 << posval)) != 0)
                 {
-                    bytenew |= (int)(System.Convert.ToUInt32(1 << bytepos));
+                    bytenew |= (int)(Convert.ToUInt32(1 << bytepos));
                 }
                 posval--;
                 bytepos--;
@@ -365,18 +426,15 @@ namespace UPRProfiler
                 }
             }
         }
-
         private void writeByte(byte value)
         {
             byteout.writeByte(value);
         }
-
         private void writeWord(int value)
         {
             writeByte((byte)((value >> 8) & 0xFF));
             writeByte((byte)((value) & 0xFF));
         }
-
         private float[] fDCTQuant(float[] data, float[] fdtbl)
         {
             float tmp0; float tmp1; float tmp2; float tmp3; float tmp4; float tmp5; float tmp6; float tmp7;
@@ -488,8 +546,6 @@ namespace UPRProfiler
             }
             return data;
         }
-
-
         private void writeAPP0()
         {
             writeWord(0xFFE0);
@@ -507,7 +563,6 @@ namespace UPRProfiler
             writeByte(0);
             writeByte(0);
         }
-
         private void writeSOF0(int width, int height)
         {
             writeWord(0xFFC0);
@@ -526,7 +581,6 @@ namespace UPRProfiler
             writeByte(0x11);
             writeByte(1);
         }
-
         private void writeDQT()
         {
             writeWord(0xFFDB);
@@ -543,7 +597,6 @@ namespace UPRProfiler
                 writeByte((byte)(UVTable[i]));
             }
         }
-
         private void writeDHT()
         {
             writeWord(0xFFC4);
@@ -590,7 +643,6 @@ namespace UPRProfiler
                 writeByte((byte)(std_ac_chrominance_values[i]));
             }
         }
-
         private void writeSOS()
         {
             writeWord(0xFFDA);
@@ -606,11 +658,6 @@ namespace UPRProfiler
             writeByte(0x3f);
             writeByte(0);
         }
-        private int[] DU = new int[64];
-        private float[] YDU = new float[64];
-        private float[] UDU = new float[64];
-        private float[] VDU = new float[64];
-
         private float processDU(float[] CDU, float[] fdtbl, float DC, BitString[] HTDC, BitString[] HTAC)
         {
             BitString EOB = HTAC[0x00];
@@ -672,7 +719,6 @@ namespace UPRProfiler
             }
             return DC;
         }
-
         private void RGB2YUV(BitmapData img, int xpos, int ypos)
         {
             int pos = 0;
@@ -698,54 +744,6 @@ namespace UPRProfiler
                 }
             }
         }
-
-        public bool isDone = false;
-        private BitmapData image;
-        private int sf = 0;
-
-        public JPGEncoder(float quality)
-        {
-
-            if (quality <= 0.0f)
-                quality = 1.0f;
-
-            if (quality > 100.0f)
-                quality = 100.0f;
-
-            if (quality < 50.0f)
-                sf = (int)(5000.0f / quality);
-            else
-                sf = (int)(200.0f - quality * 2.0f);
-
-            initHuffmanTbl();
-            initCategoryfloat();
-            initQuantTables(sf);
-        }
-
-
-        public void doEncoding(byte[] rawByte, int width, int height)
-        {
-
-            image = new BitmapData(rawByte, width, height);
-            isDone = false;
-            encode();
-
-            isDone = true;
-
-            image = null;
-        }
-#if UNITY_2018_2_OR_NEWER
-        public void doNativeEncoding(NativeArray<byte> rawByte, int width, int height)
-        {
-
-            image = new BitmapData(rawByte, width, height);
-            isDone = false;
-            encode();
-            isDone = true;
-            image = null;
-        }
-#endif
-
         private void encode()
         {
             // Initialize bit writer
@@ -789,5 +787,6 @@ namespace UPRProfiler
             writeWord(0xFFD9);
             isDone = true;
         }
+        #endregion
     }
 }
